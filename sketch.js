c08eb97240e88c6399c27690829c42ec9cb220e7
suryamishra007 <74192796+suryamishra007@users.noreply.github.com>
2021-03-23 19:57:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,81 @@
+
+var balloon,balloonI;
+var backgroundI
+var database,pos;
+
+function preload()
+{
+backgroundI = loadImage("bg.png");
+balloonI = loadImage("balloon2.png")
+
+}
+
+function setup() {
+
+  database = firebase.database();
+
+  createCanvas(1000,1000);
+
+  
+
+  balloon = createSprite(500, 1000, 50, 50);
+  balloon.addImage("moving",balloonI);
+  balloon.scale = 0.5;
+
+  var balloonpos = database.ref('balloon/position');
+  balloonpos.on ("value",readPosition);
+
+}
+
+function draw() {
+  background(backgroundI);  
+
+  textSize(20);
+  stroke(5);
+  fill("black")
+  text("USE ARROW KEY TO MOVE BALLOON",100,50)
+
+  if(pos!==undefined)
+  {
+  
+  
+
+  if(keyDown(LEFT_ARROW)){
+      writePosition(-5,0);
+  }
+  else if(keyDown(RIGHT_ARROW)){
+      writePosition(5,0);
+  }
+  else if(keyDown(UP_ARROW)){
+      writePosition(0,-5);
+      balloon.scale = balloon.scale-0.00250;
+  }
+  else if(keyDown(DOWN_ARROW)){
+      writePosition(0,+5);
+      balloon.scale = balloon.scale+0.00250;
+  }
+
+  drawSprites();
+  
+}
+}
+function writePosition(x,y){
+
+  database.ref('balloon/position').set({
+      'x':pos.x+x,
+      'y':pos.y+y
+  })
+
+ // ball.x = ball.x + x;
+ // ball.y = ball.y + y;
+
+  
+}
+
+function readPosition(data)
+{
+pos = data.val();
+balloon.x = pos.x;
+balloon.y = pos.y;   
+
+}
